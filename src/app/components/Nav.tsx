37e8768446ffce1b4e8b3180531f1d5043482702
nikolay <n_yozov@hotmail.com>
2025-11-27 18:05:22 -0500

    add navbar

---
+++ src/app/components/Nav.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Button,
+} from "@heroui/react";
+
+import Link from "next/link";
+
+const Nav = () => {
+  return (
+    <Navbar isBordered maxWidth="full">
+      <NavbarBrand>
+        <Link href="/" className="font-bold text-inherit">Tourist</Link>
+      </NavbarBrand>
+      <NavbarContent justify="end">
+        <NavbarItem>
+          <Button radius="full" className="bg-transparent">
+            Sign Up
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  );
+};
+
+export default Nav;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import { Providers } from "./providers";
 import "./globals.css";
+import Nav from "./components/Nav";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,7 +24,10 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Providers>{children}</Providers>
+        <Providers>
+          <Nav />
+          {children}
+        </Providers>
       </body>
     </html>
   );
